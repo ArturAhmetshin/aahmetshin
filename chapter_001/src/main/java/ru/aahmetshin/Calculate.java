5399304474a2a4c23aafae42ce841f9386d5da90
ArturAhmetshin <arturahm@mail.ru>
2016-11-21 19:54:59 +0300

    add Java Doc

--- chapter_001/src/main/java/ru/aahmetshin/Calculate.java
+++ chapter_001/src/main/java/ru/aahmetshin/Calculate.java
@@ -1,5 +1,11 @@
 package ru.aahmetshin;
 
+/**
+* Class Кдасс для вычисления арифметических операций + - * / 
+* @author aahmetshin
+* @since 21.11.2016
+* @version 1
+*/
 public class Calculate {
 	public static void main(String[] args) {
 	System.out.println("Hello world.");
