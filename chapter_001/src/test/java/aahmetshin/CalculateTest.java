2898a5d07740a4237234c573aa768d096d8e0e98
ArturAhmetshin <arturahm@mail.ru>
2016-11-21 21:44:32 +0300

    junit

---
+++ chapter_001/src/test/java/aahmetshin/CalculateTest.java
@@ -0,0 +1,27 @@
+package aahmetshin;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author aahmetshin
+ * @version 1
+ * @since 21.11.2016
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World.\r\n"));
+    }
+}
