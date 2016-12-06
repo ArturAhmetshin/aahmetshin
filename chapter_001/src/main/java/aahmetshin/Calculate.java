b3e466f3d6419dd871dde6e4a94de7ef4ca3e1ca
ArturAhmetshin <arturahm@mail.ru>
2016-12-06 20:11:03 +0300

    delete target

--- chapter_001/src/main/java/aahmetshin/Calculate.java
+++ chapter_001/src/main/java/aahmetshin/Calculate.java
@@ -1,14 +1,18 @@
 package aahmetshin;
 
 /**
-* Class Кдасс для вычисления арифметических операций + - * / 
+* Class Кдасс для вычисления арифметических операций + - * /.
 * @author aahmetshin
 * @since 21.11.2016
 * @version 1
 */
 public class Calculate {
+	/**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args) {
-	System.out.println("Hello world.");
+	System.out.println("Hello World.");
 }
 
 }
